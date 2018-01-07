3460dd57b10c061839ee1b4a0a166ae3094d4658
NaveenDK <naveenkolambage@gmail.com>
2018-01-07 19:28:22 +1300

    added initial files

---
+++ src/components/footerComponent/footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import  './Assets/css/defaultmin.css';
+
+
+class Footer extends Component {
+  render() {
+    return (
+       <footer>
+            Footer
+
+        </footer>
+             
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/pages/homePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import  './Assets/css/defaultmin.css';
+
+
+class Homepage extends Component {
+  render() {
+    return (
+        <div className="container-fluid">
+                Home Page content here 
+
+            </div> 
+    );
+  }
+}
+
+export default Homepage ;
