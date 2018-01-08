45de3240cf54c22d314e98a4d543868696e7cf23
NaveenDK <naveenkolambage@gmail.com>
2018-01-08 15:48:32 +1300

    Router Functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import  './Assets/css/defaultmin.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';  
+import Products from './components/pages/products'
 
 import Header from './components/headerComponent/header'
 import Footer from './components/footerComponent/footer'
@@ -8,11 +14,13 @@ import Homepage from './components/pages/homePage'
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
        <Header/>
-         <Homepage/>
-         
+           <Route exact path='/' component ={Homepage}/>
+           <Route exact path='/Products' component ={Products}/>
       </div>
+      </Router>
     );
   }
 }
--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 //import  './Assets/css/defaultmin.css';
+import {
+    Link
+} from 'react-router-dom'; 
 
 
 class Header extends Component {
@@ -12,19 +15,15 @@ class Header extends Component {
             <nav>
                 <ul>
                     <li className="first">
-                       <a href='#'>
-                       Home
-                       </a> 
+                    <Link to="/">Home</Link>
+                       
                     </li>
                     <li>
-                       <a href='#'>
-                       Products
-                       </a> 
+                    <Link to="/Products">Products</Link>
+                     
                     </li>
                     <li className="last">
-                       <a href='#'>
-                       Contact
-                                             </a> 
+                    <Link to="/">Contact</Link>
                     </li>
 
                 </ul>
